a787905ef6f9e4a7f41a7a93f96eab92a0eb3078
Stephen Cocuzza <scocuzza3@gmail.com>
2020-11-02 08:50:26 -0500

    socket io

--- controllers/stock_controller.js
+++ controllers/stock_controller.js
@@ -1,16 +1,16 @@
 
 const router = require('express').Router();
-const axios = require('axios')
+const axios = require('axios');
 const Stock = require('../models/stock');
 
-
 //INDEX
-router.get('/', async (req, res) => {
-  let stocks = await Stock.find({});
-  res.render('index.ejs', {
-      stocks
+router.get('/', async (req, res, next) => {
+    let stocks = await Stock.find({});
+    res.render('index.ejs', {
+        stocks
     });
-});
+})
+
 
 //NEW
 router.get('/new', async (req, res) => {
@@ -111,75 +111,149 @@ router.put('/:id', async (req,res)=> {
     }  )
 })
 
-async function getStockData(inputSymbol) {
-    let config = {
-        method: 'GET',
-        url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
-        params: {
-            apikey: 'TMIF9RATR89WC6J6BDOSA1PYQS7KKUBT',
-            symbol: inputSymbol
-        },
-        headers: { }
-      };
-    await axios(config)
-    .then(function (response) {
-        data = response.data
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    return data
-}
-async function createStock(data, element) {
-    element.toUpperCase()
-    return new Stock({
-        assetType: data[element].assetType,
-        assetMainType: data[element].assetMainType,
-        cusip: data[element].cusip,
-        symbol: data[element].symbol,
-        description: data[element].description,
-        bidPrice: data[element].bidPrice,
-        bidSize: data[element].bidSize,
-        bidId: data[element].bidId,
-        askPrice: data[element].askPrice,
-        askSize: data[element].askSize,
-        askId: data[element].askId,
-        lastPrice: data[element].lastPrice,
-        lastSize: data[element].lastSize,
-        lastId: data[element].lastId,
-        openPrice: data[element].openPrice,
-        highPrice: data[element].highPrice,
-        lowPrice: data[element].lowPrice,
-        bidTick: data[element].bidTick,
-        closePrice: data[element].closePrice,
-        netChange: data[element].netChange,
-        totalVolume: data[element].totalVolume,
-        quoteTimeInLong: data[element].quoteTimeInLong,
-        tradeTimeInLong: data[element].tradeTimeInLong,
-        mark: data[element].mark,
-        exchange: data[element].exchange,
-        exchangeName: data[element].exchangeName,
-        marginable: data[element].marginable,
-        shortable: data[element].shortable,
-        volatility: data[element].volatility,
-        digits: data[element].digits,
-        yearHigh: data[element]['52WkHigh'],
-        yearLow: data[element]['52WkLow'],
-        nAV: data[element].nAV,
-        peRatio: data[element].peRatio,
-        divAmount: data[element].divAmount,
-        divYield: data[element].divYield,
-        divDate: data[element].divDate,
-        securityStatus: data[element].securityStatus,
-        regularMarketLastPrice: data[element].regularMarketLastPrice,
-        regularMarketLastSize: data[element].regularMarketLastSize,
-        regularMarketNetChange: data[element].regularMarketNetChange,
-        regularMarketTradeTimeInLong: data[element].regularMarketTradeTimeInLong,
-        netPercentChangeInDouble: data[element].netPercentChangeInDouble,
-        markChangeInDouble: data[element].markChangeInDouble,
-        markPercentChangeInDouble: data[element].markPercentChangeInDouble,
-        regularMarketPercentChangeInDouble: data[element].regularMarketPercentChangeInDouble,
-        delayed: data[element].delayed
-      });
-}
 module.exports = router;
+
+// setInterval(()=>{
+//     refreshData()
+// }, 10000) 
+// async function refreshData() {
+//     let stockArray = await getCurrentSymbols();
+//     let newStockData = await getStockData(stockArray)
+//     await updateStockData(stockArray, newStockData)
+// }
+// async function getCurrentSymbols() {
+//     let stockArray = []
+//     await Stock.find({}, (err, data) => {
+//         data.forEach(element=> {
+//             stockArray.push(element.symbol)
+//         })
+//     })
+//     return stockArray
+// }
+// async function updateStockData(stockArray, newStock) {
+//     stockArray.forEach( stock=> {
+//         Stock.updateOne({symbol: stock}, 
+//             {
+//             assetType: newStock[stock].assetType,
+//             assetMainType: newStock[stock].assetMainType,
+//             cusip: newStock[stock].cusip,
+//             symbol: newStock[stock].symbol,
+//             description: newStock[stock].description,
+//             bidPrice: newStock[stock].bidPrice,
+//             bidSize: newStock[stock].bidSize,
+//             bidId: newStock[stock].bidId,
+//             askPrice: newStock[stock].askPrice,
+//             askSize: newStock[stock].askSize,
+//             askId: newStock[stock].askId,
+//             lastPrice: newStock[stock].lastPrice,
+//             lastSize: newStock[stock].lastSize,
+//             lastId: newStock[stock].lastId,
+//             openPrice: newStock[stock].openPrice,
+//             highPrice: newStock[stock].highPrice,
+//             lowPrice: newStock[stock].lowPrice,
+//             bidTick: newStock[stock].bidTick,
+//             closePrice: newStock[stock].closePrice,
+//             netChange: newStock[stock].netChange,
+//             totalVolume: newStock[stock].totalVolume,
+//             quoteTimeInLong: newStock[stock].quoteTimeInLong,
+//             tradeTimeInLong: newStock[stock].tradeTimeInLong,
+//             mark: newStock[stock].mark,
+//             exchange: newStock[stock].exchange,
+//             exchangeName: newStock[stock].exchangeName,
+//             marginable: newStock[stock].marginable,
+//             shortable: newStock[stock].shortable,
+//             volatility: newStock[stock].volatility,
+//             digits: newStock[stock].digits,
+//             yearHigh: newStock[stock]['52WkHigh'],
+//             yearLow: newStock[stock]['52WkLow'],
+//             nAV: newStock[stock].nAV,
+//             peRatio: newStock[stock].peRatio,
+//             divAmount: newStock[stock].divAmount,
+//             divYield: newStock[stock].divYield,
+//             divDate: newStock[stock].divDate,
+//             securityStatus: newStock[stock].securityStatus,
+//             regularMarketLastPrice: newStock[stock].regularMarketLastPrice,
+//             regularMarketLastSize: newStock[stock].regularMarketLastSize,
+//             regularMarketNetChange: newStock[stock].regularMarketNetChange,
+//             regularMarketTradeTimeInLong: newStock[stock].regularMarketTradeTimeInLong,
+//             netPercentChangeInDouble: newStock[stock].netPercentChangeInDouble,
+//             markChangeInDouble: newStock[stock].markChangeInDouble,
+//             markPercentChangeInDouble: newStock[stock].markPercentChangeInDouble,
+//             regularMarketPercentChangeInDouble: newStock[stock].regularMarketPercentChangeInDouble,
+//             delayed: newStock[stock].delayed
+//         }, (err) => {
+//             console.log('Update Complete');
+//         })
+//         })
+// }
+// async function getStockData(inputSymbol) {
+//     let config = {
+//         method: 'GET',
+//         url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
+//         params: {
+//             apikey: 'TMIF9RATR89WC6J6BDOSA1PYQS7KKUBT',
+//             symbol: inputSymbol.join(',')
+//         },
+//         headers: { }
+//       };
+//     await axios(config)
+//     .then(function (response) {
+//         data = response.data
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+//     return data
+// }
+// async function createStock(data, element) {
+//     element.toUpperCase()
+//     return new Stock({
+//         assetType: data[element].assetType,
+//         assetMainType: data[element].assetMainType,
+//         cusip: data[element].cusip,
+//         symbol: data[element].symbol,
+//         description: data[element].description,
+//         bidPrice: data[element].bidPrice,
+//         bidSize: data[element].bidSize,
+//         bidId: data[element].bidId,
+//         askPrice: data[element].askPrice,
+//         askSize: data[element].askSize,
+//         askId: data[element].askId,
+//         lastPrice: data[element].lastPrice,
+//         lastSize: data[element].lastSize,
+//         lastId: data[element].lastId,
+//         openPrice: data[element].openPrice,
+//         highPrice: data[element].highPrice,
+//         lowPrice: data[element].lowPrice,
+//         bidTick: data[element].bidTick,
+//         closePrice: data[element].closePrice,
+//         netChange: data[element].netChange,
+//         totalVolume: data[element].totalVolume,
+//         quoteTimeInLong: data[element].quoteTimeInLong,
+//         tradeTimeInLong: data[element].tradeTimeInLong,
+//         mark: data[element].mark,
+//         exchange: data[element].exchange,
+//         exchangeName: data[element].exchangeName,
+//         marginable: data[element].marginable,
+//         shortable: data[element].shortable,
+//         volatility: data[element].volatility,
+//         digits: data[element].digits,
+//         yearHigh: data[element]['52WkHigh'],
+//         yearLow: data[element]['52WkLow'],
+//         nAV: data[element].nAV,
+//         peRatio: data[element].peRatio,
+//         divAmount: data[element].divAmount,
+//         divYield: data[element].divYield,
+//         divDate: data[element].divDate,
+//         securityStatus: data[element].securityStatus,
+//         regularMarketLastPrice: data[element].regularMarketLastPrice,
+//         regularMarketLastSize: data[element].regularMarketLastSize,
+//         regularMarketNetChange: data[element].regularMarketNetChange,
+//         regularMarketTradeTimeInLong: data[element].regularMarketTradeTimeInLong,
+//         netPercentChangeInDouble: data[element].netPercentChangeInDouble,
+//         markChangeInDouble: data[element].markChangeInDouble,
+//         markPercentChangeInDouble: data[element].markPercentChangeInDouble,
+//         regularMarketPercentChangeInDouble: data[element].regularMarketPercentChangeInDouble,
+//         delayed: data[element].delayed
+//       });
+// }
--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
-const app = express ();
+const app = express()
 const db = mongoose.connection;
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
 //___________________
 //Port
 //___________________
@@ -52,10 +55,116 @@ app.use('/stocks', stockController)
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/' , (req, res) => {
+//   res.send('Hello World!');
+// });
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+
+async function refreshData() {
+    let stockArray = await getCurrentSymbols();
+    let newStockData = await getStockData(stockArray)
+    await updateStockData(stockArray, newStockData)
+}
+
+async function getCurrentSymbols() {
+    let stockArray = []
+    await Stock.find({}, (err, data) => {
+        data.forEach(element=> {
+            stockArray.push(element.symbol)
+        })
+    })
+    return stockArray
+}
+
+async function getStockData(symbols) {
+    let config = {
+        method: 'GET',
+        url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
+        params: {
+            apikey: 'TMIF9RATR89WC6J6BDOSA1PYQS7KKUBT',
+            symbol: symbols.join(',')
+        },
+        headers: { }
+      };
+    await axios(config)
+    .then(function (response) {
+        data = response.data
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    return data
+}
+async function updateStockData(stockArray, newStock) {
+    stockArray.forEach( stock=> {
+        Stock.updateOne({symbol: stock}, 
+            {
+            assetType: newStock[stock].assetType,
+            assetMainType: newStock[stock].assetMainType,
+            cusip: newStock[stock].cusip,
+            symbol: newStock[stock].symbol,
+            description: newStock[stock].description,
+            bidPrice: newStock[stock].bidPrice,
+            bidSize: newStock[stock].bidSize,
+            bidId: newStock[stock].bidId,
+            askPrice: newStock[stock].askPrice,
+            askSize: newStock[stock].askSize,
+            askId: newStock[stock].askId,
+            lastPrice: newStock[stock].lastPrice,
+            lastSize: newStock[stock].lastSize,
+            lastId: newStock[stock].lastId,
+            openPrice: newStock[stock].openPrice,
+            highPrice: newStock[stock].highPrice,
+            lowPrice: newStock[stock].lowPrice,
+            bidTick: newStock[stock].bidTick,
+            closePrice: newStock[stock].closePrice,
+            netChange: newStock[stock].netChange,
+            totalVolume: newStock[stock].totalVolume,
+            quoteTimeInLong: newStock[stock].quoteTimeInLong,
+            tradeTimeInLong: newStock[stock].tradeTimeInLong,
+            mark: newStock[stock].mark,
+            exchange: newStock[stock].exchange,
+            exchangeName: newStock[stock].exchangeName,
+            marginable: newStock[stock].marginable,
+            shortable: newStock[stock].shortable,
+            volatility: newStock[stock].volatility,
+            digits: newStock[stock].digits,
+            yearHigh: newStock[stock]['52WkHigh'],
+            yearLow: newStock[stock]['52WkLow'],
+            nAV: newStock[stock].nAV,
+            peRatio: newStock[stock].peRatio,
+            divAmount: newStock[stock].divAmount,
+            divYield: newStock[stock].divYield,
+            divDate: newStock[stock].divDate,
+            securityStatus: newStock[stock].securityStatus,
+            regularMarketLastPrice: newStock[stock].regularMarketLastPrice,
+            regularMarketLastSize: newStock[stock].regularMarketLastSize,
+            regularMarketNetChange: newStock[stock].regularMarketNetChange,
+            regularMarketTradeTimeInLong: newStock[stock].regularMarketTradeTimeInLong,
+            netPercentChangeInDouble: newStock[stock].netPercentChangeInDouble,
+            markChangeInDouble: newStock[stock].markChangeInDouble,
+            markPercentChangeInDouble: newStock[stock].markPercentChangeInDouble,
+            regularMarketPercentChangeInDouble: newStock[stock].regularMarketPercentChangeInDouble,
+            delayed: newStock[stock].delayed
+        }, (err) => {
+            console.log('Update Complete');
+        })
+        })
+}
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+ 
+    //Send a message after a timeout of 4seconds
+    setTimeout(function() {
+       console.log('HELLO');
+    }, 4000);
+ 
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    });
+ });
+
+http.listen(PORT, () => console.log( 'Listening on port:', PORT));
