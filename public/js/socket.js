ea30fa8f20f4ab325d2c673349d0a96ad5a85238
Stephen Cocuzza <scocuzza3@gmail.com>
2020-11-06 12:22:38 -0500

    real time color updates to percent column

--- public/js/socket.js
+++ public/js/socket.js
@@ -17,4 +17,14 @@ socket.on('testEvent', function(stocks){
         }
         
     });
+    document.querySelectorAll('#row-item').forEach( item =>{
+        let number = item.innerHTML
+        number = parseFloat(number.slice(0,-1))
+        console.log(number);
+        if(number > 0 ) {
+            item.style.backgroundColor = 'rgba(100, 215, 97)'
+        } else {
+            item.style.backgroundColor = 'rgba(255, 0, 0)'
+        }
+    })
 });
